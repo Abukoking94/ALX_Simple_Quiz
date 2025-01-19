f12054fcb10bc03dd986c9edc112ffd72051f744
Abenezer Fekadu <Abukokinggg94@gmail.com>
2025-01-19 14:38:17 +0300

    Final Calculator

--- quiz.js
+++ quiz.js
@@ -1,14 +1,13 @@
 function checkAnswer() {
-  let correctAnswer = "4";
-  let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-
-  if (userAnswer == correctAnswer) {
-    document.querySelector("#feedback").textContent = "Correct! Well done.";
-  } else {
-    document.querySelector("#feedback").textContent =
-      "That's incorrect. Try again!";
-  }
-  console.log(userAnswer);
+    let correctAnswer = "4";
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    
+    if (userAnswer == correctAnswer) {
+      document.querySelector("#feedback").textContent = "Correct! Well done.";
+    } else {
+      document.querySelector("#feedback").textContent =
+        "That's incorrect. Try again!";
+    }
 }
 
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
